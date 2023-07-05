78cbf29f9a4eb75844306c57b7b57ae754977530
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-07-04 18:32:08 -0700

    Add filtered chats

--- tggpt/app.py
+++ tggpt/app.py
@@ -2,7 +2,6 @@ import logging
 
 from aiogram import Bot, Dispatcher, types
 from os import environ
-from collections import defaultdict
 from .last_messages import LastMessages
 from .prompt import prompt
 from openai import ChatCompletion
@@ -14,14 +13,19 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=environ['BOT_TOKEN'])
 dp = Dispatcher(bot)
 
-all_messages = defaultdict(lambda: LastMessages(500))
+allowed_chats = [int(chat) for chat in environ.get("ALLOWED_CHATS", "").split(",") if chat]
+all_messages = {chat: LastMessages(500) for chat in allowed_chats}
 
 
 @dp.message_handler()
 async def message_received(message: types.Message):
+    if message.chat.id not in all_messages:
+        logging.warning("Ignoring chat %s, message from %s",
+                        message.chat.id, message.from_user.username)
+        return
     last_messages = all_messages[message.chat.id]
     last_messages.add(message.message_id,
-                                      f'{message.from_user.username}: {message.text}')
+                      f'{message.from_user.username}: {message.text}')
     mentions = [m.get_text(message.text) for m in message.entities if m.type == "mention"]
     bot = await message.bot.me
 
@@ -34,7 +38,7 @@ async def message_received(message: types.Message):
         last_messages.add(result.message_id, f'{result.from_user.username}: {result.text}')
 
 
-@dp.edited_message_handler()
+@dp.message_handler(lambda msg: msg.chat.id in all_messages)
 async def message_edited(message: types.Message):
     all_messages[message.chat.id].edit(message.message_id,
                                        f'{message.from_user.username}: {message.text}')
