a0518be2d694b9aaf6da56d80e29ae7db77a5c23
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-09-14 18:55:33 -0700

    Use gpt-4

--- tggpt/app.py
+++ tggpt/app.py
@@ -98,7 +98,7 @@ async def respond(bot: types.User, message: types.Message, last_messages):
     now = datetime.now(timezone.utc)
     p = prompt(now, bot.username, last_messages.get_all())
     placeholder = message.answer("🤖💭", reply=True)
-    completion = to_thread(ChatCompletion.create, model="gpt-3.5-turbo", temperature=0, messages=p)
+    completion = to_thread(ChatCompletion.create, model="gpt-4", temperature=0, messages=p)
     placeholder, completion = await gather(placeholder, completion)
 
     response = completion.choices[0].message.content.strip()
